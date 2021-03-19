5e30b2f11d47702ae946bb4e5f53735f309f3c1b
david.muolo <davidmuolo1@gmail.com>
2021-03-19 01:42:02 -0500

    V 0.98

--- StoreFront.UI.MVC/Models/CartItemViewModel.cs
+++ StoreFront.UI.MVC/Models/CartItemViewModel.cs
@@ -3,16 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using StoreFront.DATA.EF;
-using System.ComponentModel.DataAnnotations;
 
 
 namespace StoreFront.UI.MVC.Models
 {
     public class CartItemViewModel
     {
-        [Range(1,byte.MaxValue,ErrorMessage = "* Please enter a Quantity between 1 and 255. *")]
         public int Qty { get; set; }
-
         public MovieTitle Product { get; set; }
 
         public CartItemViewModel(int qty, MovieTitle product)
